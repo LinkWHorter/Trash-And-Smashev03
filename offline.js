e3f4fcfd607187057b6e07d0ad65ca5d8d6f047d
LinkWHorter <LinkWHorter@gmail.com>
2023-11-27 11:50:32 +0200

    Abobio: ended;

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1701078356,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/robbox-sheet0.png",
+		"images/robbox2-sheet0.png",
+		"images/tiledbackground.png",
+		"images/movebar-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
